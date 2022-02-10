f666d4b75cbbac74695b695358d24d97fc63546b
YuvrajTetarwal <yuvi56789@gmail.com>
2022-02-10 21:55:21 +0530

    removed hardcoded ROM name

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,15 @@
 #include "cpu.h"
 #include "display.h"
 
-int main(int argc, char *args[])
+int main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		std::cout<< "Usage: " << argv[0] << "<ROM>\n";
+	}
 	Emulator emulator;
 
-	//string name = "cpu/cpu_instrs";
-	//string name = "instr_timing";
-
-	//emulator.memory.load_rom("tests/" + name + ".gb");
-	//emulator.memory.load_rom("roms/Dr. Mario.gb");
-	//emulator.memory.load_rom("roms/kirby.gb");
-	//emulator.memory.load_rom("roms/tetris.gb");
-	//emulator.memory.load_rom("roms/minesweeper.gb");
-	//emulator.memory.load_rom("roms/Super Mario Land.gb");
-	//emulator.memory.load_rom("roms/cASTELIAN.gb");
-	//emulator.memory.load_rom("roms/Serpent.gb");
-
-	emulator.memory.load_rom("roms/Tetris.gb");
+	emulator.memory.load_rom(argv[1]);
 
 	emulator.run();
 
